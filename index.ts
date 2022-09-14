9f32ec468ad935a9f73069602e1b0e43ff46dc21
Filip Eller <filip.eller@aalto.fi>
2022-09-14 23:07:58 +0300

    Add book and author queries.

--- index.ts
+++ index.ts
@@ -1,18 +1,35 @@
 import { ApolloServer, gql } from 'apollo-server';
+import { books, authors } from './data';
 
 const typeDefs = gql`
   type Book {
+    id: String!
     title: String!
+    description: String
+    authors: [String!]!
+    language: String!
+    publishedDate: String
+    categories: [String]!
+    pageCount: Int
+    thumbnail: String
+    smallThumbnail: String
+  }
+  type Author {
+    name: String!
   }
   type Query {
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
 const resolvers = {
   Query: {
     allBooks: async (root: any, args: any) => {
-      return [{ title: 'book1' }, { title: 'book2' }];
+      return books;
+    },
+    allAuthors: async (root: any, args: any) => {
+      return authors;
     },
   },
 };
