bd2393272817f5b5be6881d3ead718764c0a5b4b
Filip Eller <filip.eller@aalto.fi>
2022-09-20 22:47:06 +0300

    Update context imports.

--- grqphql/context.ts
+++ grqphql/context.ts
@@ -1,20 +1,18 @@
 import getErrorMessage from '../util/getErrorMessage';
 
-const { UserInputError } = require('apollo-server');
-const { JWT_SECRET } = require('../util/config');
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
+import { UserInputError } from 'apollo-server';
+import { JWT_SECRET } from '../util/config';
+import User from '../models/User';
+import jwt from 'jsonwebtoken';
 
 const context = async ({ req }: { req: any }) => {
   try {
-    const token = req.headers.authorization || '';
-
+    const token = req.headers?.authorization || '';
     if (token.toLowerCase().startsWith('bearer ')) {
-      const decodedToken = jwt.verify(token.substring(7), JWT_SECRET);
+      const decodedToken: any = jwt.verify(token.substring(7), JWT_SECRET);
       const currentUser = await User.findByPk(decodedToken.id);
       return { currentUser };
     }
-
     return {};
   } catch (error) {
     throw new UserInputError(getErrorMessage(error), {
